501fc6573e42dc7ff798916ad80caff74b2a8f42
Sérgio da Silva Albuquerque <sergio.albuquerquefilho@upe.br>
2025-10-18 17:07:57 -0300

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,85 +1,165 @@
 package main;
 
-import java.awt.Component;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.InputStream;
-import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
+import helpz.LoadSave;
+import inputs.KeyboardListener;
+import inputs.MyMouseListener;
+import managers.TileManager;
+import scenes.Editing;
+import scenes.Menu;
+import scenes.Playing;
+import scenes.Settings;
+
 public class Game extends JFrame implements Runnable {
-    private TelaJogo gameScreen;
-    private BufferedImage img;
-    private Thread gameThread;
-    private final double FPS_SET = 120.0;
-    private final double UPS_SET = 60.0;
 
-    public Game() {
-        this.importImg();
-        this.setSize(640, 640);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLocationRelativeTo((Component)null);
-        this.gameScreen = new TelaJogo(this.img);
-        this.add(this.gameScreen);
-        this.setVisible(true);
-    }
+	private GameScreen gameScreen;
+	private Thread gameThread;
 
-    private void importImg() {
-        InputStream is = this.getClass().getResourceAsStream("spriteatlas.png");
-        try {
-            this.img = ImageIO.read(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	private final double FPS_SET = 120.0;
+	private final double UPS_SET = 60.0;
 
-    private void start() {
-        this.gameThread = new Thread(this);
-        this.gameThread.start();
-    }
+	// Classes
+	private Render render;
+	private Menu menu;
+	private Playing playing;
+	private Settings settings;
+	private Editing editing;
 
-    private void updateGame() {
-        gameScreen.update();
-    }
+	private TileManager tileManager;
 
-    public static void main(String[] args) {
-        Game game = new Game();
-        game.start();
-    }
+	public Game() {
 
-    @Override
-    public void run() {
-        double timePerFrame = 1_000_000_000.0 / FPS_SET;
-        double timePerUpdate = 1_000_000_000.0 / UPS_SET;
+		initClasses();
+		createDefaultLevel();
 
-        long lastFrame = System.nanoTime();
-        long lastUpdate = System.nanoTime();
-        long lastTimeCheck = System.currentTimeMillis();
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		add(gameScreen);
+		pack();
+		setVisible(true);
 
-        int frames = 0;
-        int updates = 0;
+	}
 
-        while(true) {
-            long now = System.nanoTime();
+	private void createDefaultLevel() {
+		int[] arr = new int[400];
+		for (int i = 0; i < arr.length; i++)
+			arr[i] = 0;
 
-            if ((now - lastFrame) >= timePerFrame) {
-                this.repaint();
-                lastFrame = now;
-                frames++;
-            }
+		LoadSave.CreateLevel("new_level", arr);
 
-            if ((now - lastUpdate) >= timePerUpdate) {
-                this.updateGame();
-                lastUpdate = now;
-                updates++;
-            }
+	}
 
-            if (System.currentTimeMillis() - lastTimeCheck >= 1000L) {
-                System.out.println("FPS: " + frames + " | UPS: " + updates);
-                frames = 0;
-                updates = 0;
-                lastTimeCheck = System.currentTimeMillis();
-            }
-        }
-    }
+	private void initClasses() {
+		tileManager = new TileManager();
+		render = new Render(this);
+		gameScreen = new GameScreen(this);
+		menu = new Menu(this);
+		playing = new Playing(this);
+		settings = new Settings(this);
+		editing = new Editing(this);
+
+	}
+
+	private void start() {
+		gameThread = new Thread(this) {
+		};
+
+		gameThread.start();
+	}
+
+	private void updateGame() {
+		switch (GameStates.gameState) {
+		case EDIT:
+			editing.update();
+			break;
+		case MENU:
+			break;
+		case PLAYING:
+			playing.update();
+			break;
+		case SETTINGS:
+			break;
+		default:
+			break;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Game game = new Game();
+		game.gameScreen.initInputs();
+		game.start();
+
+	}
+
+	@Override
+	public void run() {
+
+		double timePerFrame = 1000000000.0 / FPS_SET;
+		double timePerUpdate = 1000000000.0 / UPS_SET;
+
+		long lastFrame = System.nanoTime();
+		long lastUpdate = System.nanoTime();
+		long lastTimeCheck = System.currentTimeMillis();
+
+		int frames = 0;
+		int updates = 0;
+
+		long now;
+
+		while (true) {
+			now = System.nanoTime();
+
+			// Render
+			if (now - lastFrame >= timePerFrame) {
+				repaint();
+				lastFrame = now;
+				frames++;
+			}
+
+			// Update
+			if (now - lastUpdate >= timePerUpdate) {
+				updateGame();
+				lastUpdate = now;
+				updates++;
+			}
+
+			if (System.currentTimeMillis() - lastTimeCheck >= 1000) {
+				System.out.println("FPS: " + frames + " | UPS: " + updates);
+				frames = 0;
+				updates = 0;
+				lastTimeCheck = System.currentTimeMillis();
+			}
+
+		}
+
+	}
+
+	// Getters and setters
+	public Render getRender() {
+		return render;
+	}
+
+	public Menu getMenu() {
+		return menu;
+	}
+
+	public Playing getPlaying() {
+		return playing;
+	}
+
+	public Settings getSettings() {
+		return settings;
+	}
+
+	public Editing getEditor() {
+		return editing;
+	}
+
+	public TileManager getTileManager() {
+		return tileManager;
+	}
+
 }
