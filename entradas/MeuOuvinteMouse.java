ee6b3d615b63b01068e3ebda0836cf2a58e72564
annebeatrizbt <annebeatriz-04@hotmail.com>
2025-12-09 23:15:34 -0300

    Update MeuOuvinteMouse.java

--- entradas/MeuOuvinteMouse.java
+++ entradas/MeuOuvinteMouse.java
@@ -9,81 +9,81 @@ import principal.EstadosJogo;
 
 public class MeuOuvinteMouse implements MouseListener, MouseMotionListener {
 
-    private Jogo jogo;
+	private Jogo jogo;
 
-    public MeuOuvinteMouse(Jogo jogo) {
-        this.jogo = jogo;
-    }
+	public MeuOuvinteMouse(Jogo jogo) {
+		this.jogo = jogo;
+	}
 
-    @Override
-    public void mouseDragged(MouseEvent e) { // Nome da AWT em Inglês
-        switch (EstadosJogo.estadoJogo) {
-            // Chamada interna do seu jogo em Português
-            case MENU -> jogo.getMenu().mouseArrastado(e.getX(), e.getY());
-            case JOGANDO -> jogo.getJogando().mouseArrastado(e.getX(), e.getY());
-            case CONFIGURACOES -> jogo.getConfiguracoes().mouseArrastado(e.getX(), e.getY());
-            case EDITAR -> jogo.getEditor().mouseArrastado(e.getX(), e.getY());
-            default -> {}
-        }
-    }
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		switch (EstadosJogo.estadoJogo) {
+			case MENU -> jogo.getMenu().mouseArrastado(e.getX(), e.getY());
+			case JOGANDO -> jogo.getJogando().mouseArrastado(e.getX(), e.getY());
+			case CONFIGURACOES -> jogo.getConfiguracoes().mouseArrastado(e.getX(), e.getY());
+			case EDITAR -> jogo.getEditor().mouseArrastado(e.getX(), e.getY());
+			case FIM_DE_JOGO -> jogo.getFimDeJogo().mouseArrastado(e.getX(), e.getY());
+			default -> {}
+		}
+	}
 
-    @Override
-    public void mouseMoved(MouseEvent e) { // Nome da AWT em Inglês
-        switch (EstadosJogo.estadoJogo) {
-            // Chamada interna do seu jogo em Português
-            case MENU -> jogo.getMenu().mouseMovido(e.getX(), e.getY());
-            case JOGANDO -> jogo.getJogando().mouseMovido(e.getX(), e.getY());
-            case CONFIGURACOES -> jogo.getConfiguracoes().mouseMovido(e.getX(), e.getY());
-            case EDITAR -> jogo.getEditor().mouseMovido(e.getX(), e.getY());
-            default -> {}
-        }
-    }
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		switch (EstadosJogo.estadoJogo) {
+			case MENU -> jogo.getMenu().mouseMovido(e.getX(), e.getY());
+			case JOGANDO -> jogo.getJogando().mouseMovido(e.getX(), e.getY());
+			case CONFIGURACOES -> jogo.getConfiguracoes().mouseMovido(e.getX(), e.getY());
+			case EDITAR -> jogo.getEditor().mouseMovido(e.getX(), e.getY());
+			case FIM_DE_JOGO -> jogo.getFimDeJogo().mouseMovido(e.getX(), e.getY());
+			default -> {}
+		}
+	}
 
-    @Override
-    public void mouseClicked(MouseEvent e) { // Nome da AWT em Inglês
-        if (e.getButton() == MouseEvent.BUTTON1) {
-            switch (EstadosJogo.estadoJogo) {
-                // Chamada interna do seu jogo em Português
-                case MENU -> jogo.getMenu().mouseClicado(e.getX(), e.getY());
-                case JOGANDO -> jogo.getJogando().mouseClicado(e.getX(), e.getY());
-                case CONFIGURACOES -> jogo.getConfiguracoes().mouseClicado(e.getX(), e.getY());
-                case EDITAR -> jogo.getEditor().mouseClicado(e.getX(), e.getY());
-                default -> {}
-            }
-        }
-    }
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (e.getButton() == MouseEvent.BUTTON1) {
+			switch (EstadosJogo.estadoJogo) {
+				case MENU -> jogo.getMenu().mouseClicado(e.getX(), e.getY());
+				case JOGANDO -> jogo.getJogando().mouseClicado(e.getX(), e.getY());
+				case CONFIGURACOES -> jogo.getConfiguracoes().mouseClicado(e.getX(), e.getY());
+				case EDITAR -> jogo.getEditor().mouseClicado(e.getX(), e.getY());
+				case FIM_DE_JOGO -> jogo.getFimDeJogo().mouseClicado(e.getX(), e.getY());
+				default -> {}
+			}
+		}
+	}
 
-    @Override
-    public void mousePressed(MouseEvent e) { // Nome da AWT em Inglês
-        switch (EstadosJogo.estadoJogo) {
-            // Chamada interna do seu jogo em Português
-            case MENU -> jogo.getMenu().mousePressionado(e.getX(), e.getY());
-            case JOGANDO -> jogo.getJogando().mousePressionado(e.getX(), e.getY());
-            case CONFIGURACOES -> jogo.getConfiguracoes().mousePressionado(e.getX(), e.getY());
-            case EDITAR -> jogo.getEditor().mousePressionado(e.getX(), e.getY());
-            default -> {}
-        }
-    }
+	@Override
+	public void mousePressed(MouseEvent e) {
+		switch (EstadosJogo.estadoJogo) {
+			case MENU -> jogo.getMenu().mousePressionado(e.getX(), e.getY());
+			case JOGANDO -> jogo.getJogando().mousePressionado(e.getX(), e.getY());
+			case CONFIGURACOES -> jogo.getConfiguracoes().mousePressionado(e.getX(), e.getY());
+			case EDITAR -> jogo.getEditor().mousePressionado(e.getX(), e.getY());
+			case FIM_DE_JOGO -> jogo.getFimDeJogo().mousePressionado(e.getX(), e.getY());
+			default -> {}
+		}
+	}
 
-    @Override
-    public void mouseReleased(MouseEvent e) { // Nome da AWT em Inglês
-        switch (EstadosJogo.estadoJogo) {
-            // Chamada interna do seu jogo em Português
-            case MENU -> jogo.getMenu().mouseSolto(e.getX(), e.getY());
-            case JOGANDO -> jogo.getJogando().mouseSolto(e.getX(), e.getY());
-            case CONFIGURACOES -> jogo.getConfiguracoes().mouseSolto(e.getX(), e.getY());
-            case EDITAR -> jogo.getEditor().mouseSolto(e.getX(), e.getY());
-            default -> {}
-        }
-    }
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		switch (EstadosJogo.estadoJogo) {
+			case MENU -> jogo.getMenu().mouseSolto(e.getX(), e.getY());
+			case JOGANDO -> jogo.getJogando().mouseSolto(e.getX(), e.getY());
+			case CONFIGURACOES -> jogo.getConfiguracoes().mouseSolto(e.getX(), e.getY());
+			case EDITAR -> jogo.getEditor().mouseSolto(e.getX(), e.getY());
+			case FIM_DE_JOGO -> jogo.getFimDeJogo().mouseSolto(e.getX(), e.getY());
+			default -> {}
+		}
+	}
 
-    @Override
-    public void mouseEntered(MouseEvent e) { // Nome da AWT em Inglês
-        // Não utilizado
-    }
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		
+	}
 
-    @Override
-    public void mouseExited(MouseEvent e) { // Nome da AWT em Inglês
-        // Não utilizado
-    }
+	@Override
+	public void mouseExited(MouseEvent e) {
+		
+	}
 }
