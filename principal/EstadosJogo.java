46c2e7c6d8fd71faa23a1a0351581d8bda41d57c
annebeatrizbt <annebeatriz-04@hotmail.com>
2025-12-09 23:01:20 -0300

    Update EstadosJogo.java

--- principal/EstadosJogo.java
+++ principal/EstadosJogo.java
@@ -2,12 +2,16 @@ package principal;
 
 public enum EstadosJogo {
 
-    JOGANDO, MENU, CONFIGURACOES, EDITAR;
+    JOGANDO,
+    MENU,
+    CONFIGURACOES,
+    EDITAR,
+    FIM_DE_JOGO; 
 
     public static EstadosJogo estadoJogo = MENU;
 
-    public static void DefinirEstadoJogo(EstadosJogo estado) {
+    
+    public static void DefinirEstadoJogo(EstadosJogo estado) { 
         estadoJogo = estado;
     }
-
 }
