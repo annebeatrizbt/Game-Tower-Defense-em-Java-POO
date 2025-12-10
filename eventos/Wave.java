9c6c4e47790001929f42fa0dad055fb996796adf
annebeatrizbt <annebeatriz-04@hotmail.com>
2025-12-09 23:22:09 -0300

    Update Wave.java

--- eventos/Wave.java
+++ eventos/Wave.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 
 public class Wave {
 
-    private ArrayList<Integer> listaInimigos;
+	private ArrayList<Integer> listaInimigos;
 
-    public Wave(ArrayList<Integer> listaInimigos) {
-        this.listaInimigos = listaInimigos;
-    }
+	public Wave(ArrayList<Integer> listaInimigos) {
+		this.listaInimigos = listaInimigos;
+	}
 
-    public ArrayList<Integer> getListaInimigos() {
-        return listaInimigos;
-    }
+	public ArrayList<Integer> getListaInimigos() {
+		return listaInimigos;
+	}
 }
